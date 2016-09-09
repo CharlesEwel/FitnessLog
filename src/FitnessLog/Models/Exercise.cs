833f21753218400ee9601309398162d622d7327e
Charles Ewel <Charles.Ewel@gmail.com>
2016-09-09 16:36:22 -0700

    progress towards implementing table of exercises

---
+++ src/FitnessLog/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitnessLog.Models
+{
+    [Table("Exercises")]
+    public class Exercise
+    {
+        [Key]
+        public int ExerciseId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
