25ef5ca1e595889dd2b59f8bff1b8038d060b660
Charles Ewel <Charles.Ewel@gmail.com>
2016-09-23 16:15:58 -0700

    got testdb working

--- FitnessLog.Tests/UserTest.cs
+++ FitnessLog.Tests/UserTest.cs
@@ -1,4 +1,9 @@
-﻿using FitnessLog.Models;
+﻿using FitnessLog.Controllers;
+using FitnessLog.Models;
+using FitnessLog.Models.Repositories;
+using FitnessLog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +14,9 @@ namespace FitnessLog.Tests
 {
     public class UserTest
     {
+        EFEntryRepository db = new EFEntryRepository(new TestDbContext());
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         [Fact]
         public void GetAgeWeightandHeightTest()
         {
@@ -21,6 +29,20 @@ namespace FitnessLog.Tests
             Assert.Equal(26, age);
             Assert.Equal(188, weight);
             Assert.Equal(74, height);
+        }
+        [Fact]
+        public void DB_CreateNewUser_Test()
+        {
+            // Arrange
+            UsersController controller = new UsersController(_userManager, _signInManager, db);
+            RegisterViewModel newUserRegistration = new RegisterViewModel { Email = "test@test.com", Password="F92gh67Z?", ConfirmPassword = "F92gh67Z?", Age = 26, Weight = 188, Height = 74 };
+
+            // Act
+            controller.Create(newUserRegistration);
+            var collection = (controller.Index() as ViewResult).ViewData.Model as IEnumerable<ApplicationUser>;
+
+            // Assert
+            Assert.NotEmpty(collection);
         }
     }
 }
